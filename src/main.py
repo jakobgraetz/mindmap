38173a698b3535aa0cc47a38857ed952733e7cbb
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-07 15:05:52 +0100

    initialized flask app

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
 # Author Jakob Grätz
 # Version 07-03-2024 (DD-MM-YYYY)
 # (C) Jakob Grätz - 2024
+
+# Import necessary Libraries
+from flask import Flask, render_template
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route for the root URL "/"
+@app.route("/")
+def index():
+    # Render the index.html template
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # Run the Flask app
+    app.run(debug=True)

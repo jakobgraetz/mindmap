7cb3347d482da2ddad40486d1a29690dad730cda
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-09 10:03:11 +0100

    frontend update

--- src/static/main.js
+++ src/static/main.js
@@ -3,3 +3,12 @@
 * Version 07-03-2024 (DD-MM-YYYY)
 * (C) Jakob Grätz - 2024
 */
+
+if (document.getElementById("idea-submit")) {
+    document.getElementById("idea-submit").addEventListener("click", () => {
+        let idea = document.getElementById("idea-input").value;
+        if (idea != "") {
+            alert("clicked - value: " + idea);
+        };
+    });
+};
